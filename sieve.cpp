1887f728481f3e26bd31041e70f874bb0c394f77
ifahim1000 <34990550+ifahim1000@users.noreply.github.com>
2020-12-22 22:30:44 +0600

    Create sieve.cpp

---
+++ sieve.cpp
@@ -0,0 +1,26 @@
+#define mx 1000005
+vector<int>prime;
+ 
+bool vis[mx];
+ 
+void sieve() {
+    int x=sqrt((int)mx);
+    for(int i=3; i<=x; i+=2) 
+    {
+        if(vis[i]==0) 
+        {
+            for(int j=i*i; j<mx; j+=2*i)
+            {
+                vis[j]=1;
+            }
+        }
+    }
+    prime.push_back(2);
+    for(int i=3; i<mx; i+=2)
+    {
+        if(vis[i]==0)
+        {
+            prime.push_back(i);
+        }
+    }
+}
