bdd1821ed49c9b94a4979e009d3036e5dd15583b
ifahim1000 <34990550+ifahim1000@users.noreply.github.com>
2021-08-11 02:26:45 +0600

    Update Sum of divisors.cpp

--- Sum of divisors.cpp
+++ Sum of divisors.cpp
@@ -3,6 +3,18 @@
 char stat[100000001];
 vector<long long >prime;
 
+
+template <typename T>
+T modpow(T base, T exp, T modulus) {
+  base %= modulus;
+  T result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = (result * base) % modulus;
+    base = (base * base) % modulus;
+    exp >>= 1;
+  }
+  return result;
+}
 /*------bitwise sieve-----*/
 void generate () {
     long long  sqrtn = sqrt ( MAX );
